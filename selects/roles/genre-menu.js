e1de8d445a28e31b82b89ad9c4658710a7be664d
morphe63550 <111448960+morphe63550@users.noreply.github.com>
2022-09-15 19:29:10 +0200

    Delete selects/roles directory

--- selects/roles/genre-menu.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'genre-menu',
-    async runInteraction(client, interaction) {
-        await interaction.member.roles.add(interaction.values[0]);
-        await interaction.reply({ content: 'Le rôle as bien était rajouter ', ephemeral: true });
-    }
-};
